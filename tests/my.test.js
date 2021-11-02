3279c63cb0a95b54ce2291947f70635b366c50c0
n-norrr <norlandbatista@ufl.edu>
2021-11-02 15:36:34 -0400

    Update my.test.js

--- tests/my.test.js
+++ tests/my.test.js
@@ -1,4 +1,5 @@
 const { registerUser } = require('../services/user.js');
+const User = require('../models/User.js');
 const db = require('./db');
 
 describe('insert', () => {
@@ -15,11 +16,13 @@ describe('insert', () => {
     })
 
     it('create user', async () => {
-        const user = await registerUser(
+        await registerUser(
             "kylemcgee23@gmail.com",
             "Kyle23",
             "password23"
-        )
+        );
+
+        const user = await User.findOne({ email: 'kylemcgee23@gmail.com' });
 
         expect(user.email).toEqual("kylemcgee23@gmail.com");
         expect(user.username).toEqual("Kyle23");
